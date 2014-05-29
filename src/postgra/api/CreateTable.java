e1f9df19a26c37a31d71adf6e134f24b7c3c3766
Evan Summers <evan.summers@gmail.com>
2014-05-29 09:18:33 +0200

    create user and table placeholders

---
+++ src/postgra/api/CreateTable.java
@@ -0,0 +1,41 @@
+/*
+ * Source https://github.com/evanx by @evanxsummers
+ */
+package postgra.api;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import postgra.app.PostgraApp;
+import postgra.app.PostgraEntityService;
+import postgra.app.PostgraHttpx;
+import postgra.app.PostgraHttpxHandler;
+import postgra.jdbc.RowSets;
+import vellum.jx.JMap;
+
+/**
+ *
+ * @author evan.summers
+ */
+public class CreateTable implements PostgraHttpxHandler {
+    
+    private static Logger logger = LoggerFactory.getLogger(CreateTable.class); 
+
+    @Override
+    public JMap handle(PostgraApp app, PostgraHttpx httpx, PostgraEntityService es) throws Exception {
+        logger.info("handle", httpx.getPathArgs());
+        Connection connection = RowSets.getLocalPostgresConnection("template1", "postgra", "postgra");
+        try {
+            String sql = "create database " + httpx.getPathString(2);
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.execute();
+            JMap response = new JMap();
+            response.put("pathArgs", httpx.getPathArgs());
+            response.put("sql", sql);
+            return response;
+        } finally {
+            RowSets.close(connection);
+        }
+    }
+}
